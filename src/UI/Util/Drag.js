a5d8f328b0e9c7b614f2dde59c9c6f8e5297642e
Zakai Hamilton <zakaihamilton@gmail.com>
2023-08-20 16:46:08 +0300

    refactored component

--- src/UI/Util/Drag.js
+++ src/UI/Util/Drag.js
@@ -1,30 +1,30 @@
 import { useCallback, useEffect, useState } from "react";
-import { createState, withState } from "src/Core/Base/State";
+import { createState } from "src/Core/Base/State";
 
 const Drag = createState("Drag");
 
-export function useMoveDrag() {
+const useDrag = (initialCb, moverCb, prop) => {
     const [handle, setHandle] = useState(null);
     const state = Drag.useState();
-    const ref = useCallback(ref => {
+    const ref = useCallback((ref) => {
         setHandle(ref);
     }, []);
     const handleMouseDown = useCallback((e) => {
         state.target = handle;
-        state.offset = { x: e.clientX - state.region.left, y: e.clientY - state.region.top };
-        state.moving = true;
-    }, [handle, state]);
-    const handleMouseUp = useCallback((e) => {
+        state.offset = initialCb(e, state);
+        state[prop] = true;
+    }, [state, handle, initialCb, prop]);
+    const handleMouseUp = useCallback(() => {
         state.target = null;
         state.offset = null;
-        state.moving = false;
-    }, [state]);
+        state[prop] = false;
+    }, [state, prop]);
     const handleMouseMove = useCallback((e) => {
-        if (state.target && state.moving) {
-            state.region.left = e.clientX - state.offset.x;
-            state.region.top = e.clientY - state.offset.y;
+        if (state.target && state[prop]) {
+            moverCb(e, state);
         }
-    }, [state]);
+    }, [state, prop, moverCb]);
+
     useEffect(() => {
         if (!handle) {
             return;
@@ -36,47 +36,34 @@ export function useMoveDrag() {
             handle.removeEventListener("mousedown", handleMouseDown);
             window.document.removeEventListener("mouseup", handleMouseUp);
             window.document.removeEventListener("mousemove", handleMouseMove);
-        }
-    }, [handleMouseDown, handleMouseMove, handleMouseUp, handle]);
+        };
+    }, [handle, handleMouseDown, handleMouseMove, handleMouseUp]);
+
     return ref;
+};
+
+export function useMoveDrag() {
+    const initialCb = useCallback((e, state) => ({
+        x: e.clientX - state.region.left,
+        y: e.clientY - state.region.top,
+    }), []);
+    const moverCb = useCallback((e, state) => {
+        state.region.left = e.clientX - state.offset.x;
+        state.region.top = e.clientY - state.offset.y;
+    }, []);
+    return useDrag(initialCb, moverCb, "moving");
 }
 
 export function useResizeDrag() {
-    const [handle, setHandle] = useState(null);
-    const state = Drag.useState();
-    const ref = useCallback(ref => {
-        setHandle(ref);
+    const initialCb = useCallback((e, state) => ({
+        x: e.clientX - state.region.width,
+        y: e.clientY - state.region.height,
+    }), []);
+    const moverCb = useCallback((e, state) => {
+        state.region.width = e.clientX - state.offset.x;
+        state.region.height = e.clientY - state.offset.y;
     }, []);
-    const handleMouseDown = useCallback((e) => {
-        state.target = handle;
-        state.offset = { x: e.clientX - (state.region.width), y: e.clientY - (state.region.height) };
-        state.resizing = true;
-    }, [handle, state]);
-    const handleMouseUp = useCallback((e) => {
-        state.target = null;
-        state.offset = null;
-        state.resizing = false;
-    }, [state]);
-    const handleMouseMove = useCallback((e) => {
-        if (state.target && state.resizing) {
-            state.region.width = e.clientX - state.offset.x;
-            state.region.height = e.clientY - state.offset.y;
-        }
-    }, [state]);
-    useEffect(() => {
-        if (!handle) {
-            return;
-        }
-        handle.addEventListener("mousedown", handleMouseDown);
-        window.document.addEventListener("mouseup", handleMouseUp);
-        window.document.addEventListener("mousemove", handleMouseMove);
-        return () => {
-            handle.removeEventListener("mousedown", handleMouseDown);
-            window.document.removeEventListener("mouseup", handleMouseUp);
-            window.document.removeEventListener("mousemove", handleMouseMove);
-        }
-    }, [handleMouseDown, handleMouseMove, handleMouseUp, handle]);
-    return ref;
+    return useDrag(initialCb, moverCb, "resizing");
 }
 
 export default Drag;
