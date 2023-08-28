e8e5a24960338f364cdc2f25e6f0b5c0c66b0ea9
zakaihamilton <zakaihamilton@gmail.com>
2023-08-28 13:21:18 +0300

    hide taskbar when fullscreen

--- src/UI/Desktop/Taskbar.js
+++ src/UI/Desktop/Taskbar.js
@@ -1,9 +1,19 @@
 import { withTheme } from "../Util/Theme";
 import styles from "./Taskbar.module.scss";
 import Items from "./Taskbar/Items";
+import Windows from "src/UI/Windows";
+import { joinClassNames } from "src/Core/Util/Styles";
 
 function Taskbar() {
-    return <div className={styles.root}>
+    const windows = Windows.State.useState();
+    const list = windows.list;
+
+    const visible = useMemo(() => {
+        const hasFullscreen = list?.some(item => item?.fullscreen);
+        return !hasFullscreen;
+    }, [list]);
+
+    return <div className={joinClassNames(styles.root, visible && styles.visible)}>
         <Items />
     </div>;
 }
