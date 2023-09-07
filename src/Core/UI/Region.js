acaea81fb909139347551f6c0c797fa47dc1680b
zakaihamilton <zakaihamilton@gmail.com>
2023-09-07 18:01:11 +0300

    use region function

--- src/Core/UI/Region.js
+++ src/Core/UI/Region.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { createState } from "src/Core/Base/State";
 
 export function createRegion(displayName) {
@@ -17,4 +17,28 @@ export function createRegion(displayName) {
     Region.State = createState(displayName);
 
     return Region;
+}
+
+export function useRegion(target) {
+    const [counter, setCounter] = useState(0);
+    useEffect(() => {
+        if (!target) {
+            return;
+        }
+        const observer = new ResizeObserver(() => {
+            setCounter(counter => counter + 1);
+        });
+        observer.observe(target);
+        return () => {
+            observer.unobserve(target);
+        };
+    }, [target]);
+    const region = useMemo(() => {
+        if (!target) {
+            return null;
+        }
+        return target.getBoundingClientRect();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [target, counter]);
+    return region;
 }
