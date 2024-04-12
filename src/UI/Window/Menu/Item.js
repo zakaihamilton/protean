534a2b6b7df722e2916427ae742546cffa32efe3
Zakai Hamilton <zakaihamilton@gmail.com>
2024-04-12 13:00:06 +0300

    style fix

--- src/UI/Window/Menu/Item.js
+++ src/UI/Window/Menu/Item.js
@@ -2,12 +2,10 @@ import { useClasses } from "src/Core/Util/Styles";
 import styles from "./Item.module.scss";
 import Menu from "../Menu";
 import { useCallback, useMemo } from "react";
-import Window from "src/UI/Window";
 import Node from "src/Core/Base/Node";
 
 export default function Item({ label, id, items, onClick, checked }) {
     const menu = Menu.State.useState();
-    const window = Window.State.useState();
     const classes = useClasses(styles);
     const selected = menu?.selected === id;
     const itemClassName = classes({ item: true, selected });
@@ -18,12 +16,6 @@ export default function Item({ label, id, items, onClick, checked }) {
         checked,
         visible: typeof checked !== "undefined"
     });
-    const style = useMemo(() => {
-        return {
-            "--accent-background": window.accentBackground || "darkblue",
-            "--accent-color": window.accentColor || "white"
-        }
-    }, [window.accentBackground, window.accentColor]);
     const onClickItem = useCallback(() => {
         let close = false;
         if (onClick) {
@@ -48,7 +40,7 @@ export default function Item({ label, id, items, onClick, checked }) {
         }
     }, [id, menu, onClick, selected]);
     return <div className={styles.root}>
-        <div className={itemClassName} style={style} onClick={onClickItem}>
+        <div className={itemClassName} onClick={onClickItem}>
             <div className={checkClassName} />
             <div className={labelClassName}>
                 {label}
