7d838d1821b2a6f4a831213c79e8c377f8253e14
Zakai Hamilton <zakaihamilton@gmail.com>
2024-05-01 19:18:56 +0300

    bug fix

--- src/Core/Base/Array.js
+++ src/Core/Base/Array.js
@@ -1,22 +1,18 @@
 export function moveItem(arr, fromIndex, toIndex) {
-    if (fromIndex < 0 || fromIndex >= arr.length || toIndex < 0 || toIndex >= arr.length) {
-        return [...arr];
+    if (fromIndex < 0) {
+        fromIndex = arr.length - fromIndex;
     }
-
-    if (fromIndex === toIndex) {
-        return [...arr];
+    if (toIndex < 0) {
+        toIndex = arr.length - toIndex;
     }
-
     const copiedArr = [...arr];
+    if (fromIndex === toIndex) {
+        return copiedArr;
+    }
 
     const itemToMove = copiedArr.splice(fromIndex, 1)[0];
 
-    if (arr.length === 2 && fromIndex === 0 && toIndex === 1) {
-        copiedArr.push(itemToMove);
-    } else {
-        const adjustedToIndex = toIndex < fromIndex ? toIndex : toIndex - 1;
-        copiedArr.splice(adjustedToIndex, 0, itemToMove);
-    }
+    copiedArr.splice(toIndex, 0, itemToMove);
 
     return copiedArr;
 }
