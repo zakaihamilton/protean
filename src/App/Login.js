590f16427092de60b467537c10e7af83edd261f0
Zakai Hamilton <zhamilto@synamedia.com>
2025-11-06 20:47:34 +0200

    move login resources to separate file

--- src/App/Login.js
+++ src/App/Login.js
@@ -9,77 +9,7 @@ import { createState } from "src/Core/Base/State";
 import { useClasses } from "src/Core/Util/Styles";
 import Resources from "src/Core/UI/Resources";
 import Button from "src/UI/Widgets/Button";
-
-const resources = {
-    TITLE: {
-        eng: "Login",
-        heb: "התחברות"
-    },
-    USER_NOT_FOUND: {
-        eng: "User not found",
-        heb: "משתמש לא נמצא"
-    },
-    INCORRECT_PASSWORD: {
-        eng: "Incorrect password",
-        heb: "סיסמה שגויה"
-    },
-    PASSWORD_TOO_SHORT: {
-        eng: "Password too short",
-        heb: "הסיסמה קצרה מדי"
-    },
-    USER_ID: {
-        eng: "User ID",
-        heb: "מזהה משתמש"
-    },
-    PASSWORD: {
-        eng: "Password",
-        heb: "סיסמה"
-    },
-    LOGIN: {
-        eng: "Login",
-        heb: "התחבר"
-    },
-    LOGOUT: {
-        eng: "Logout",
-        heb: "התנתק"
-    },
-    CHANGE_PASSWORD: {
-        eng: "Change Password",
-        heb: "שנה סיסמה"
-    },
-    ENTER_USER_ID: {
-        eng: "Enter User ID",
-        heb: "הזן מזהה משתמש"
-    },
-    ENTER_PASSWORD: {
-        eng: "Enter Password",
-        heb: "הזן סיסמה"
-    },
-    ENTER_NEW_PASSWORD: {
-        eng: "Enter New Password",
-        heb: "הזן סיסמה חדשה"
-    },
-    LOGGING_IN: {
-        eng: "Logging in...",
-        heb: "מתחבר..."
-    },
-    LOGIN_TITLE: {
-        eng: "Login",
-        heb: "התחברות"
-    },
-    LOGIN_SUCCESS: {
-        eng: "Login successful",
-        heb: "התחברות בוצעה בהצלחה"
-    },
-    ENTER_USER_ID_AND_PASSWORD: {
-        eng: "Please enter your User ID and Password",
-        heb: "אנא הזן מזהה משתמש וסיסמה"
-    },
-    UNKNOWN_ERROR: {
-        eng: "An unknown error occurred",
-        heb: "אירעה שגיאה לא ידועה"
-    }
-};
+import resources from './Login.res';
 
 export default function Login() {
     const lookup = Resources.useLookup();
